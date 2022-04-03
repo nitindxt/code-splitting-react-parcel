6f637ac5c509118ae4e872a1ab368a608c8f8eb4
Nitin Dixit <nitinajaydixit@gmail.com>
2022-04-03 13:23:41 +0530

    add code-splitting through lazy/suspense

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import { render } from "react-dom";
-import SearchParams from "./SearchParams";
-import { StrictMode, useState } from "react";
+// import SearchParams from "./SearchParams"; //static import executes at build-time not run-time
+import { StrictMode, useState, lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Details from "./Details";
+// import Details from "./Details";
 import ThemeContext from "./ThemeContext";
+
+const Details = lazy(() => import("./Details")); //dynamic js import(return promise) with lazy func in react
+const SearchParams = lazy(() => import("./SearchParams")); //The React.lazy function lets you render a dynamic import as a regular component.
 
 const App = () => {
   const theme = useState("darkblue");
   return (
     <StrictMode>
-      <ThemeContext.Provider value={theme}>
-        <BrowserRouter>
-          <header>
-            <Link to="/">Adopt Me!</Link>
-          </header>
-          <Routes>
-            <Route path="/details/:id" element={<Details />} />
-            <Route path="/" element={<SearchParams />} />
-          </Routes>
-        </BrowserRouter>
-      </ThemeContext.Provider>
+      <Suspense fallback={<h2>loading, wait a sec you weirdo 😜</h2>}>
+        <ThemeContext.Provider value={theme}>
+          <BrowserRouter>
+            <header>
+              <Link to="/">Adopt Me!</Link>
+            </header>
+            <Routes>
+              <Route path="/details/:id" element={<Details />} />
+              <Route path="/" element={<SearchParams />} />
+            </Routes>
+          </BrowserRouter>
+        </ThemeContext.Provider>
+      </Suspense>
     </StrictMode>
   );
 };
